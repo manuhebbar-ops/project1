0e9f558fc9a1dc08152aebdb410b572e6e5bac50
manuhebbar-ops <manuhebbars@gmail.com>
2024-07-19 12:12:13 +0000

    Commit odd_or-even and fibonacci series

---
+++ fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm;
+
+    // Ask the user to enter the number of terms
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (int i = 1; i <= n; ++i) {
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+
+    return 0;
+}
+
